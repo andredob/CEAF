35b934e9360b94322532fa376791b76089173cc0
André Garcia Dobermann <andre-gd@hotmail.com>
2019-01-02 00:40:57 -0200

    poucas coisas

--- CEAF/src/ga/Population.java
+++ CEAF/src/ga/Population.java
@@ -11,16 +11,28 @@ import java.util.List;
 public class Population {
     
     List population = new ArrayList<Population>();
+    List elite = new ArrayList<Population>();
+    int popSize;
     
-    public Population(int size){
-        createPop(size);
+    
+    public Population(int popSize){
+        createPop(popSize);
+        this.popSize = popSize;
     }
     
     public Population reprod(){
         return null;
     }
     
+    public void elite(){
+        
+    }
+    
     public void createPop(int size){
+        
+    }
+    
+    public void rouletWheel(){
         
     }
     
